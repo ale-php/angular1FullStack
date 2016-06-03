9bfd42c1dd60f7f0b371f2beb98f1cc9df0bc43b
ale-php <alexandre@progs.net.br>
2016-06-02 22:31:54 -0300

    base pronta

---
+++ components/app.js
@@ -0,0 +1,12 @@
+
+(function(){
+    
+angular.module('app', ['ngNewRouter','app.home'])
+  .controller('AppController', ['$router', AppController]);
+
+AppController.$routeConfig([
+  {path: '/', component: 'home' }
+]);
+function AppController ($router) {}
+ 
+})();
